68c6efb5bf2b51fdf4a9ff6a5e432de7a1499371
Daniel <danielbenicio123@gmail.com>
2022-01-28 17:49:32 -0300

    challenge

--- desafio1.js
+++ desafio1.js
@@ -1,16 +1,16 @@
 for(let i = 1; i<=100; i++) {
   if((i % 5) == 0  && (i % 3) == 0) {
-    console.log(i + ' fizz - buzz');
+    console.log(i + ' FizzBuzz');
     continue
   }
 
   if((i % 3) == 0 ) {
-    console.log(i + ' - fizz');
+    console.log(i + ' - Fizz');
     continue
   }
 
   if((i % 5) == 0 ) {
-    console.log(i + ' - buzz');
+    console.log(i + ' - Buzz');
     continue
   }
 };
